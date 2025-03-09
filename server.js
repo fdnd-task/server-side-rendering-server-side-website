6541efc0bc3a7b877c66c5caef33b6a47158b5ca
Krijn Hoetmer <krijn@qontent.nl>
2025-03-09 23:53:25 +0100

    Update boilerplate

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ console.log('Hieronder moet je waarschijnlijk nog wat veranderen')
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
 const app = express()
 
+// Maak werken met data uit formulieren iets prettiger
+app.use(express.urlencoded({extended: true}))
+
 // Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
 // Bestanden in deze map kunnen dus door de browser gebruikt worden
 app.use(express.static('public'))
